fec37a8bc5953c70ad264a44dd04be4e78529b56
dyego <dyegoaurelio@gmail.com>
2022-05-01 12:58:48 +0000

    ajustando code-server

--- next.config.js
+++ next.config.js
@@ -5,26 +5,28 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 
 const isCodeServer = process.env.CODE_SERVER === 'true'
 
-const codeServerProps = isCodeServer
-  ? {
-      basePath: '/absproxy/3454'
-    }
-  : {}
+const withCodeServerProps = (baseProps) => {
+  if (!isCodeServer) return baseProps
 
-module.exports = withBundleAnalyzer({
-  staticPageGenerationTimeout: 300,
-  images: {
-    domains: [
-      'www.notion.so',
-      'notion.so',
-      'images.unsplash.com',
-      'pbs.twimg.com',
-      'abs.twimg.com',
-      's3.us-west-2.amazonaws.com'
-    ],
-    formats: ['image/avif', 'image/webp'],
-    dangerouslyAllowSVG: true,
-    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
-  }
-  ...codeServerProps,
-})
+  baseProps.basePath = '/absproxy/3454'
+  return baseProps
+}
+
+module.exports = withCodeServerProps(
+  withBundleAnalyzer({
+    staticPageGenerationTimeout: 300,
+    images: {
+      domains: [
+        'www.notion.so',
+        'notion.so',
+        'images.unsplash.com',
+        'pbs.twimg.com',
+        'abs.twimg.com',
+        's3.us-west-2.amazonaws.com'
+      ],
+      formats: ['image/avif', 'image/webp'],
+      dangerouslyAllowSVG: true,
+      contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;"
+    }
+  })
+)
